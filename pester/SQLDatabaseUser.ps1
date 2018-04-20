8b263a3a72be1f4ad5d229104ca403f2d4091a90
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-04-21 00:00:55 +0100

    check if a user exist in database

--- pester/SQLDatabaseUser.ps1
+++ pester/SQLDatabaseUser.ps1
@@ -1,6 +1,6 @@
 ﻿function SQLDatabaseUser ($databaseUser) {
             
-    $sqlResult = Invoke-Sqlcmd -Query "SELECT * FROM sys.syslogins WHERE name = '$databaseUser' " -ConnectionString "server=EDPIAIRO\EDP17;database=master;integrated security=sspi;"
+    $sqlResult = Invoke-Sqlcmd -Query "SELECT * FROM sys.syslogins WHERE name = '$databaseUser' " -ConnectionString "server=EDPIAIRO\EDP17;database=master;Integrated Security=True;"
         
     if ($sqlResult -ne $null){
     
