e86e11fdbf6df7e2b79644503ccb397844b6648e
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-04-20 22:08:07 +0100

    octopus api key update

--- nuget-push_db-app.ps1
+++ nuget-push_db-app.ps1
@@ -1,6 +1,7 @@
 Param(
 	[Parameter(Mandatory=$true)][string]$resourceName,
-	[Parameter(Mandatory=$true)][string]$buildNumber)
+	[Parameter(Mandatory=$true)][string]$buildNumber,
+	[Parameter(Mandatory=$true)][string]$apiKey)
 	
 $packExtension = ".nupkg"	
 $packPath = "..\output\" + $resourceName
@@ -10,7 +11,7 @@ $packFullPath = $packPath + "." +$buildNumber + $packExtension
 Write-Output $packFullPath
 
 #push from VSTS to Octopus Deploy
-nuget push $packFullPath -Source http://localhost:8090/nuget/packages -ApiKey API-RSKZD2UYKHXXRR8SFHEA0KSH5S 
+nuget push $packFullPath -Source http://localhost:8090/nuget/packages -ApiKey $apiKey 
 
 
 exit $LastExitCode
