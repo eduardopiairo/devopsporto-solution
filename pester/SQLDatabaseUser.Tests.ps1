3c53e97c89b2ab6a7015ddd0d4dbd7c5dbfe58e5
edpiairo-ux360 <eduardopiairo@gmail.com>
2018-04-20 23:49:27 +0100

    check if a user exist in database

---
+++ pester/SQLDatabaseUser.Tests.ps1
@@ -0,0 +1,21 @@
+﻿# param(
+#     [Parameter(Mandatory=$true)][string]$databaseUser,
+#     [Parameter(Mandatory=$true)][string]$databasePass)
+
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+
+
+Describe "SQL-Database-User" {
+        
+    Context "TEST"{
+        It "Check if 'eduardo.piairo' exists on DevOpsPorto-Solution-db" {
+            SQLDatabaseUser 'eduardo.piairo' | Should Be $true
+        }
+    }
+
+    
+}
---
+++ pester/SQLDatabaseUser.ps1
@@ -0,0 +1,14 @@
+﻿function SQLDatabaseUser ($databaseUser) {
+            
+    $sqlResult = Invoke-Sqlcmd -Query "SELECT * FROM sys.syslogins WHERE name = '$databaseUser' " -ConnectionString "server=EDPIAIRO\EDP17;database=master;integrated security=sspi;"
+        
+    if ($sqlResult -ne $null){
+    
+        $true
+                
+    }
+    else {
+        $false
+    }
+
+}
